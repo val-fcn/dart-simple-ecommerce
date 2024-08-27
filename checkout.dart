1854db90652b2a652dab088bd3fa203fd3635c2b
valentin faucon <valentin.faucon@gmail.com>
2024-08-27 19:59:43 +0200

    add checkout feature

--- checkout.dart
+++ checkout.dart
@@ -24,6 +24,7 @@ class Item {
 
 class Cart {
 final Map<int, Item> _items = {};
+
   void addProduct(Product product) {
     final item = _items[product.id];
     if (item == null) {
@@ -32,6 +33,7 @@ final Map<int, Item> _items = {};
       _items[product.id] = Item(product: product, quantity: item.quantity + 1);
     }
   }
+  bool get isEmpty => _items.isEmpty;
 
   double total() => _items.values
   .map((item) => item.price)
@@ -70,8 +72,10 @@ void main () {
       }
     } else if (line == 'v') {
       print(cart);
-    } else if (line == 'C') {
-       // TODO:.Implement
+    } else if (line == 'c') {
+      if(checkout(cart)) {
+        break;
+      }
     }
   }
 }
@@ -87,4 +91,30 @@ for (var product in allProducts) {
 }
 print('Not found');
 return null;
+}
+
+bool checkout(Cart cart){
+if (cart.isEmpty) {
+  print('Cart is empty');
+  return false;
+}
+final total = cart.total();
+print('Total: \$$total');
+stdout.write('Payment in cash: ');
+final line = stdin.readLineSync();
+if (line == null || line.isEmpty){
+  return false;
+}
+final paid = double.tryParse(line);
+if (paid == null) {
+  return false;
+}
+if (paid >= total) {
+  final change = paid - total;
+  print('Change: \$${change.toStringAsFixed(2)}');
+  return true;
+} else {
+  print('Not enough cash.');
+  return false;
+}
 }
