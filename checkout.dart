0330ed5e780e179f484f28765b5d6c6c8c542561
valentin faucon <valentin.faucon@gmail.com>
2024-08-27 19:48:33 +0200

    add to cart feature

--- checkout.dart
+++ checkout.dart
@@ -5,6 +5,9 @@ class Product {
   final int id;
   final String name;
   final double price;
+
+  String get displayName => '($initial)${name.substring(1)}: \$$price';
+  String get initial => name.substring(0, 1);
 }
 
 class Item {
@@ -13,10 +16,35 @@ class Item {
   final int quantity;
 
   double get price => quantity * product.price;
-}
+
+  @override
+  String toString() => '$quantity x ${product.name}: \$$price';
+  }
+
 
 class Cart {
-  //TODO: Items.collection.
+final Map<int, Item> _items = {};
+  void addProduct(Product product) {
+    final item = _items[product.id];
+    if (item == null) {
+      _items[product.id] = Item(product: product, quantity: 1);
+    } else {
+      _items[product.id] = Item(product: product, quantity: item.quantity + 1);
+    }
+  }
+
+  double total() => _items.values
+  .map((item) => item.price)
+  .reduce((a, b) => a + b);
+
+@override
+String toString(){
+  if (_items.isEmpty) {
+    return 'Cart is empty';
+  }
+  final itemizedList = _items.values.map((item) => item.toString()).join('\n');
+  return '------\n$itemizedList\nTotal: \$${total()}\n------';
+}
 }
 
 const allProducts = [
@@ -29,15 +57,34 @@ const allProducts = [
 ];
 
 void main () {
-  while (true) {
+  final cart = Cart()
+;  while (true) {
     stdout.write('What do you want to do? (v)iew items, (a)dd tim, (c)heckout: ');
     final line = stdin.readLineSync();
     if (line == 'a') {
-      // TODO:.Implement
+      final product = chooseProduct();
+      if (product != null) {
+        cart.addProduct(product);
+        print(cart);
+
+      }
     } else if (line == 'v') {
-       // TODO:.Implement
+      print(cart);
     } else if (line == 'C') {
        // TODO:.Implement
     }
   }
+}
+
+Product? chooseProduct(){
+final productsList = allProducts.map((product) => product.displayName).join('\n');
+stdout.write('Available products: \n$productsList\nYour choice: ');
+final line = stdin.readLineSync();
+for (var product in allProducts) {
+  if (product.initial == line) {
+    return product;
+  }
+}
+print('Not found');
+return null;
 }
